d3349c58197cf7e0fdbb48e4b4b46c0088409bc0
manish8code <8manishgupta8@gmail.com>
2026-05-29 17:25:56 +0530

    model trainer is implented

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -50,5 +53,9 @@ if __name__=='__main__':
     train_data,test_data=obj.inititate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    r2_score=modeltrainer.initiate_model_trainer(train_arr,test_arr)
+    print("R2 score is ", r2_score)
  
